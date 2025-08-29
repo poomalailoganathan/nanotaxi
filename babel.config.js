1c83e29e2c5d1cc9b0ac68a8e72937e39cde666b
Gokulraja L P <82310500+gokulrajalp@users.noreply.github.com>
2025-08-29 20:27:26 +0530

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: [
-    'react-native-worklets/plugin',
-    // Add other plugins here if needed
-  ],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-worklets/plugin'], // <-- update here!
+  };
 };
